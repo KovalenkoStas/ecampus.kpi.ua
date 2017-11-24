e062e3414da0d43b8eb1763f8d4e92f99387181c
Stanislav Kovalenko <stanislav.kovalenko.dev@gmail.com>
2017-11-24 12:22:54 +0200

    Fixed incorrect sorting for Ukrainian symbols

--- app/scripts/filters/orderObjectBy.js
+++ app/scripts/filters/orderObjectBy.js
@@ -12,13 +12,24 @@ angular
 
       filtered.sort(function(a, b) {
         switch (field) {
-          case 'blockName': return a.blockName.localeCompare(b.blockName);
-          case 'nameUkr': return a.nameUkr.localeCompare(b.nameUkr);
-          case 'okr': return a.okr.localeCompare(b.okr);
+          case 'blockName':
+            return a.blockName.localeCompare(b.blockName);
+          case 'nameUkr':
+            return a.nameUkr.localeCompare(b.nameUkr);
+          case 'okr':
+            return a.okr.localeCompare(b.okr);
+          case 'disciplineName':
+            return a.disciplineName.localeCompare(b.disciplineName);
+          case 'lecturer':
+            return a.lecturer.localeCompare(b.lecturer);
+          case 'attestation':
+            return a.attestation.localeCompare(b.attestation);
         }
       });
 
-      if (reverse) filtered.reverse();
+      if (reverse) {
+        filtered.reverse();
+      }
       return filtered;
     };
   });
